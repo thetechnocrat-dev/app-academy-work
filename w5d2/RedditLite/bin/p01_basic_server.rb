854fb222b06ae940e454a25998f3f272d8a2cb14
mcmenemy <mr_mcmenemy@yahoo.com>
2016-02-02 14:30:17 -0800

    make url_path_app

--- w5d2/RedditLite/bin/p01_basic_server.rb
+++ w5d2/RedditLite/bin/p01_basic_server.rb
@@ -1,1 +1,23 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write('Hello world!')
+  res.finish
+end
+
+url_path_app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  app_path = req.path
+  res.write("#{app_path}")
+  res.finish
+end
+
+Rack::Server.start(
+  app: url_path_app,
+  Port: 3000
+)
