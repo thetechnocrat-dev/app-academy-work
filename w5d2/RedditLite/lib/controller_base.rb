716545634bbae47994c7bd33d20115cbcc23efc3
mcmenemy <mr_mcmenemy@yahoo.com>
2016-02-02 15:21:29 -0800

    create ControllerBasic redirect and render

--- w5d2/RedditLite/lib/controller_base.rb
+++ w5d2/RedditLite/lib/controller_base.rb
@@ -5,24 +5,43 @@ require_relative './session'
 
 class ControllerBase
   attr_reader :req, :res, :params
-
+  attr_accessor :already_built_response
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    unless already_built_response?
+      self.already_built_response = true
+      self.res.header["location"] = url
+      self.res.status = 302
+    else
+      raise "already rendered"
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    unless already_built_response?
+      self.res.header['Content-Type'] = content_type
+      self.already_built_response = true
+      self.res.write(content)
+    else
+      raise "already rendered"
+    end
   end
+
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
@@ -37,4 +56,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
