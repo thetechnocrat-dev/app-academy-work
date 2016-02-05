ee785db97d6cc64ce862ec3f4c5f77eff03f383e
mcmenemy <mr_mcmenemy@yahoo.com>
2016-02-05 13:54:07 -0800

    create myBind

---
+++ myBind.js
@@ -0,0 +1,42 @@
+Function.prototype.myBind = function () {
+  var that = arguments[0];
+  var args = [];
+  for (var i = 1; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+  var func = this;
+  return function() {
+    if (arguments.length > 0) {
+      func.apply(that, arguments);
+    } else {
+      func.apply(that, args);
+    }
+  };
+};
+
+function flick (times) {
+  console.log(this.name + ' ' + times + " flicks " );
+}
+
+function Lamp() {
+   this.name = "a lamp";
+}
+
+var turnOn = function() {
+   console.log("Turning on " + this.name);
+};
+
+var lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+var boundTurnOn = turnOn.bind(lamp);
+var myBoundTurnOn = turnOn.myBind(lamp);
+var boundFlick = flick.myBind(lamp, 5);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
+boundFlick();
+boundFlick(4);
+boundFlick(3);
+boundFlick();
