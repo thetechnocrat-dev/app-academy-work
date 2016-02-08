3b0e0fa38de42ece58fcd3fd57c97205c6c6cdcf
mcmenemy <mr_mcmenemy@yahoo.com>
2016-02-08 13:41:07 -0800

    Independently redevelop binding JS function

--- arguments_exercises.js
+++ arguments_exercises.js
@@ -8,4 +8,54 @@ function sum (nums) {
   return output;
 }
 
-console.log(sum(1,2,3,4,5));
+Function.prototype.myBind = function(context) {
+  var func = this; //referring to "says"\
+  var args = [].slice.call(arguments); // arguments passed when declaring it
+
+  var residue = args.slice(1);
+  return function() {
+    console.log(residue);
+    var fixed = residue.concat([].slice.call(arguments));
+
+    console.log(arguments);
+
+    return func.apply(context, fixed);
+  };
+};
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype.says = function (sound, person) {
+  console.log(this.name + " says " + sound + " to " + person + "!");
+  return true;
+};
+
+var markov = new Cat("Markov");
+var breakfast = new Cat("Breakfast");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+markov.says.myBind(breakfast, "meow", "Kush")();
+// Breakfast says meow to Kush!
+// true
+
+markov.says.myBind(breakfast)("meow", "a tree");
+// Breakfast says meow to a tree!
+// true
+
+
+var boundMarkov = markov.says.myBind(breakfast); // decloared it
+boundMarkov("meow", "a tree"); // called it
+
+markov.says.myBind(breakfast, "meow")("Markov");
+// Breakfast says meow to Markov!
+// true
+
+var notMarkovSays = markov.says.myBind(breakfast);
+notMarkovSays("meow", "me");
+// Breakfast says meow to me!
+// true
