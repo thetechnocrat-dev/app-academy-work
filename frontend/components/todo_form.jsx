ac9faba3675e67a3db0e1554fe1033ffacf29f7f
mcmenemy <mr_mcmenemy@yahoo.com>
2016-02-16 16:53:24 -0800

    begins implementation of TodoForm

---
+++ frontend/components/todo_form.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var TodoForm = React.createClass({
+  getInitialState: function () {
+    return(
+      {title: "", body: ""}
+    );
+  },
+
+  updateTitle: function() {
+    
+  }
+});
+
+
+module.exports = TodoForm;
